185eaee926f11064529c548bb7373bd51f8cf476
Sanket006 <148228922+Sanket006@users.noreply.github.com>
2025-11-03 12:25:09 +0530

    Refactor student database creation script

--- Database/student-rds.sql
+++ Database/student-rds.sql
@@ -1,11 +1,13 @@
-create database studentapp;
-use studentapp;
-CREATE TABLE if not exists students(student_id INT NOT NULL AUTO_INCREMENT,
-        student_name VARCHAR(100) NOT NULL,
+CREATE DATABASE IF NOT EXISTS studentapp;
+USE studentapp;
+
+CREATE TABLE IF NOT EXISTS students (
+    student_id INT NOT NULL AUTO_INCREMENT,
+    student_name VARCHAR(100) NOT NULL,
     student_addr VARCHAR(100) NOT NULL,
-        student_age VARCHAR(3) NOT NULL,
-        student_qual VARCHAR(20) NOT NULL,
-        student_percent VARCHAR(10) NOT NULL,
-        student_year_passed VARCHAR(10) NOT NULL,
-        PRIMARY KEY (student_id)
+    student_age INT NOT NULL,
+    student_qual VARCHAR(20) NOT NULL,
+    student_percent DECIMAL(5,2) NOT NULL,
+    student_year_passed YEAR NOT NULL,
+    PRIMARY KEY (student_id)
 );
